2ae1bd55c9dbefe0e015e612b305956380ddc16f
CKawara <ndutakawara2@gmail.com>
2022-08-21 03:42:18 +0300

    adjust model

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.social.urls')),
 ]
 
 # To handle the react routes
